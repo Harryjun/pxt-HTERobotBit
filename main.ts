fd72eb6d40d77a46219526e1e15518928fc8f8cf
HarryJun <flying_jun@outlook.com>
2018-06-05 15:11:07 +0800

    remod

--- main.ts
+++ main.ts
@@ -11,6 +11,26 @@ namespace HTERobot{
     const SUBADR2 = 0x03
     const SUBADR3 = 0x04
     const PRESCALE = 0xFE
+    const LED0_ON_L = 0x06
+    const LED0_ON_H = 0x07
+    const LED0_OFF_L = 0x08
+    const LED0_OFF_H = 0x09
+    const ALL_LED_ON_L = 0xFA
+    const ALL_LED_ON_H = 0xFB
+    const ALL_LED_OFF_L = 0xFC
+    const ALL_LED_OFF_H = 0xFD
+
+    const STP_CHA_L = 2047
+    const STP_CHA_H = 4095
+
+    const STP_CHB_L = 1
+    const STP_CHB_H = 2047
+
+    const STP_CHC_L = 1023
+    const STP_CHC_H = 3071
+
+    const STP_CHD_L = 3071
+    const STP_CHD_H = 1023
 
 
 
